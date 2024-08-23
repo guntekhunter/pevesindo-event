70a428e8b1e73fc27d633ed4a91b5ee51e29bc54
guntekhunter <guntek2000@gmail.com>
2024-08-23 22:34:02 +0800

    fix: navbar

--- app/component/Navabar.tsx
+++ app/component/Navabar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navabar() {
     return (
-        <div className='w-full flex justify-around py-[1rem] border-b-[1px]'>
+        <div className='w-full flex justify-around py-[1rem] border-b-[1px] bg-white'>
             <div className='w-[90%] justify-center flex'>
                 <Image src="/logo.png" alt='' height={300} width={300} className='w-[8rem]' />
             </div>
